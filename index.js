03038ba58ab3a056a89a9effdb7d486f05ce9d4c
Bryan English <bryan@bryanenglish.com>
2015-01-10 13:49:21 -0800

    callbackify + tap

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function decorate(Model) {
   var self = this;
 
   Model.find = function () {
-    var args = Array.prototype.slice.call(arguments);
+    var cb, args = Array.prototype.slice.call(arguments);
+    if (typeof args[args.length-1] === 'function') cb = args.pop();
     args.unshift(Model.name);
-    return self.smash.find.apply(self.smash, args)
+    var p = self.smash.find.apply(self.smash, args)
     .then(function(result){
       result.forEach(function(item) {
         item.__proto__ = Model.prototype;
@@ -21,17 +22,21 @@ function decorate(Model) {
       });
       return result;
     });
+    if (cb) {
+      p.then(function(r) { cb(null, r)}, function(e) { cb(e)});
+    }
+    return p;
   };
 
-  Model.prototype.save = function () {
-    return self.smash.save(this);
+  Model.prototype.save = function (cb) {
+    return self.smash.save(this, cb);
   };
 
-  Model.prototype.create = function () {
-    return self.smash.create(Model.name, this);
+  Model.prototype.create = function (cb) {
+    return self.smash.create(Model.name, this, cb);
   };
 
-  Model.prototype.delete = function () {
-    return self.smash.delete(this);
+  Model.prototype.delete = function (cb) {
+    return self.smash.delete(this, cb);
   };
 };
--- test.js
+++ test.js
@@ -1,34 +1,85 @@
 var ActiveSmash = require('./index');
 var assert = require('assert');
 var smashify = ActiveSmash(require('mongosmash')(require('nedb')));
+var test = require('tap').test;
 
 function Thing() {}
 
 smashify(Thing);
 
-var t = new Thing();
+test('promises', function (t) {
+  t.plan(5);
 
-t.foo = 5;
-t.bar = 6;
+  var thing = new Thing();
 
-var id;
+  thing.foo = 5;
+  thing.bar = 6;
 
-t.create()
-.then(function(result) {
-  id = result._id;
-  assert.equal(result.foo, 5);
-  assert.equal(result.bar, 6);
-  result.foo = 8;
-  return result.save();
-})
-.then(function() {
-  return Thing.find({_id: id});
-})
-.then(function(results) {
-  assert.equal(results[0].foo, 8);
-  return results[0].delete();
-}).then(function() {
-  console.log('it all works!');
-}).catch(function(e){
-  console.error(e.stack);
+  var id;
+
+  thing.create()
+  .then(function(result) {
+    id = result._id;
+    t.equal(result.foo, 5, 'create');
+    t.equal(result.bar, 6, 'create');
+    result.foo = 8;
+    return result.save();
+  })
+  .then(function() {
+    t.pass('save');
+    return Thing.find({_id: id});
+  })
+  .then(function(results) {
+    t.equal(results[0].foo, 8, 'find');
+    return results[0].delete();
+  })
+  .then(function() {
+    return Thing.find({_id: id});
+  })
+  .then(function(results) {
+    t.equal(results.length, 0, 'delete');
+  })
+  .catch(function(e){
+    t.error(e);
+    t.end();
+  });
+});
+
+test('callbacks', function (t) {
+  t.plan(10);
+
+  var thing = new Thing();
+
+  thing.foo = 5;
+  thing.bar = 6;
+
+  var id;
+
+  thing.create(function (err, result) {
+    t.error(err, 'no err');
+
+    id = result._id;
+    t.equal(result.foo, 5, 'create');
+    t.equal(result.bar, 6, 'create');
+    result.foo = 8;
+
+    result.save(function (err) {
+      t.error(err, 'no err');
+
+      t.pass('save');
+
+      Thing.find({_id: id}, function(err, results) {
+        t.error(err, 'no err');
+
+        t.equal(results[0].foo, 8, 'find');
+        results[0].delete(function(err) {
+          t.error(err, 'no err');
+          Thing.find({_id: id},function(err, results) {
+            t.error(err, 'no err');
+            t.equal(results.length, 0, 'delete');
+          });
+        });
+      });
+    });
+  });
 });
